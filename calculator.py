2ae768f39056fc6a65deddb39b581154a3e7ff5e
hendersona06 <45071096+hendersona06@users.noreply.github.com>
2019-06-28 22:21:55 +0100

    partially working functions. need to work on divide

--- calculator.py
+++ calculator.py
@@ -5,45 +5,108 @@ window = Tk()
 window.geometry('500x600')
 window.title('Calculator')
 window.configure(background='black')
-number = []
+global commandline
 commandline = []
 
-def addnum(num,number):
-    number.append(num)
-    print(number)
-    return number
+def addnum(num):
+    global commandline
+    if len(commandline) < 1:
+        commandline.append([])
+    if type(commandline[-1]) == str:
+        commandline.append([])
+    commandline[-1].append(str(num))
+    output.config(text=commandline)
 
-def operation(op,number):
-    commandline.append((''))
-    for n in number:
-        commandline[-1] = commandline[-1] + str(n)
-    commandline[-1] = int(commandline[-1])
+def operation(op):
+    global commandline
+    if [commandline[-1] != o for o in ['+','-','*','/','=']]:
+        tempnum = ''
+        for c in commandline[-1]:
+            tempnum = tempnum + str(c)
+        commandline[-1] = int(tempnum)
+        commandline.append(op)
+        output.config(text=commandline)
 
-    commandline.append(op)
+def calculate():
+    global commandline
+    # pop last entity if operator
+    if type(commandline[-1]) == str:
+        commandline.pop(-1)
+
+    # combine numbers
+    if type(commandline[-1]) == list:
+        tempnum = ''        # try to get down to one line
+        for c in commandline[-1]:
+            tempnum = tempnum + str(c)
+        commandline[-1] = int(tempnum)
     
-    print(commandline)
-        
+    # looking at bodmas as well
+    com = 0
+    while com < len(commandline) - 1:
+        if commandline[com] == '/':
+            operation = commandline[com + 1] / commandline[com - 1]
+            commandline.pop(com+1)
+            commandline.pop(com)
+            commandline.pop(com-1)
+            commandline.insert(com-1,str(operation))   # index,element
+            com = 0
+
+        if commandline[com] == '*':
+            operation = commandline[com - 1] * commandline[com + 1]
+            commandline.pop(com + 1)
+            commandline.pop(com)
+            commandline.pop(com - 1)
+            commandline.insert(com-1,str(operation))
+            com = 0
+
+        com += 1
+            
+    com = 0
+    while com < len(commandline) - 1:
+        if commandline == '+':
+            operation = commandline[com - 1] + commandline[com + 1]
+            commandline.pop(com + 1)
+            commandline.pop(com)
+            commandline.pop(com - 1)
+            commandline.insert(com-1,str(operation))
+            com = 0
+            
+        if commandline == '-':
+            operation = commandline[com - 1] - commandline[com + 1]
+            commandline.pop(com + 1)
+            commandline.pop(com)
+            commandline.pop(com - 1)
+            commandline.insert(com-1,str(operation))
+            com = 0
+
+        com += 1
+            
+    for com in range(len(commandline)):
+        if commandline[com] == '+' or com == '-':
+            pass
+            # do these things
+    output.config(text=commandline)
     
 
-button0 = Button(window, text='0', bg='black',fg='white',command=lambda:addnum(0,number))
-button1 = Button(window, text='1', bg='black',fg='white',command=lambda:addnum(1,number))
-button2 = Button(window, text='2', bg='black',fg='white',command=lambda:addnum(2,number))
-button3 = Button(window, text='3', bg='black',fg='white',command=lambda:addnum(3,number))
-button4 = Button(window, text='4', bg='black',fg='white',command=lambda:addnum(4,number))
-button5 = Button(window, text='5', bg='black',fg='white',command=lambda:addnum(5,number))
-button6 = Button(window, text='6', bg='black',fg='white',command=lambda:addnum(6,number))
-button7 = Button(window, text='7', bg='black',fg='white',command=lambda:addnum(7,number))
-button8 = Button(window, text='8', bg='black',fg='white',command=lambda:addnum(8,number))
-button9 = Button(window, text='9', bg='black',fg='white',command=lambda:addnum(9,number))
+button0 = Button(window, text='0', bg='black',fg='white',command=lambda:addnum(0))
+button1 = Button(window, text='1', bg='black',fg='white',command=lambda:addnum(1))
+button2 = Button(window, text='2', bg='black',fg='white',command=lambda:addnum(2))
+button3 = Button(window, text='3', bg='black',fg='white',command=lambda:addnum(3))
+button4 = Button(window, text='4', bg='black',fg='white',command=lambda:addnum(4))
+button5 = Button(window, text='5', bg='black',fg='white',command=lambda:addnum(5))
+button6 = Button(window, text='6', bg='black',fg='white',command=lambda:addnum(6))
+button7 = Button(window, text='7', bg='black',fg='white',command=lambda:addnum(7))
+button8 = Button(window, text='8', bg='black',fg='white',command=lambda:addnum(8))
+button9 = Button(window, text='9', bg='black',fg='white',command=lambda:addnum(9))
 
 buttondecimal = Button(window, text='.', bg='black', fg='white')
 buttondel = Button(window, text='del', bg='black', fg='white')
 
-buttonequal = Button(window, text='=', bg='red', fg='white')
-buttonplus = Button(window, text='+', bg='black', fg='white',command=lambda:operation('+',number))
-buttonminus = Button(window, text='-', bg='black', fg='white',command=lambda:operation('-',number))
-buttonmultiply = Button(window, text='*', bg='black', fg='white',command=lambda:operation('*',number))
-buttondivide = Button(window, text='/', bg='black', fg='white',command=lambda:operation('/',number))
+buttonequal = Button(window, text='=', bg='red', fg='white',command=lambda:calculate())
+buttonplus = Button(window, text='+', bg='black', fg='white',command=lambda:operation('+'))
+buttonminus = Button(window, text='-', bg='black', fg='white',command=lambda:operation('-'))
+buttonmultiply = Button(window, text='*', bg='black', fg='white',command=lambda:operation('*'))
+buttondivide = Button(window, text='/', bg='black', fg='white',command=lambda:operation('/'))
 
 
 output = Label(window, text='Output', bg='white',fg='black')
